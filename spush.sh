b6411015a993cb4d1c47b612b5f46b8cf7165181
TomoeMami <trembleafterme@outlook.com>
2021-10-12 14:30:37 +0800

    [脚本]自动上传于 2021年10月12日 14:30

--- spush.sh
+++ spush.sh
@@ -2,7 +2,7 @@
 #datime=$(date "+%Y-%m-%d %H:%M")
 datime=$(date "+%Y年%m月%d日 %H:%M")
 git add .
-git commit -m "上传于 $datime"
+git commit -m "[脚本]自动上传于 $datime"
 echo "git commit: 上传于 $datime"
 git push origin master
 echo "finished..."
